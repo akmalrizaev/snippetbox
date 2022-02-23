9062e137f47f76c6dd85ab861437bb60070f7567
Akmal Rizayev <akmaljon.rizayev@gmail.com>
2022-02-23 20:15:32 +0500

    The http.Error Shortcut

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Write([]byte("Hello from Snippetbox"))
+
 }
 
 // Add a showSnippet handler function.
@@ -30,18 +31,10 @@ func createSnippet(w http.ResponseWriter, r *http.Request) {
 	// Use r.Method to check whether the request is using POST or not. Note that
 	// http.MethodPost is a constant equal to the string "POST".
 	if r.Method != http.MethodPost {
-
-		// Use the Header().Set() method to add an 'Allow: POST' header to the
-		// response header map. The first parameter is the header name, and
-		// the second parameter is the header value.
 		w.Header().Set("Allow", http.MethodPost)
-
-		// If it's not, use the w.WriteHeader() method to send a 405 status
-		// code and the w.Write() method to write a "Method Not Allowed"
-		// response body. We then return from the function so that the
-		// subsequent code is not executed.
-		w.WriteHeader(405)
-		w.Write([]byte("Method Not Allowed"))
+		// Use the http.Error() function to send a 405 status code and "Method Not
+		// Allowed" string as the response body.
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
@@ -57,4 +50,5 @@ func main() {
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
+
 }
