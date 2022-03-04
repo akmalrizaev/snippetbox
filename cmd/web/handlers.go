e7560febf74262489d4dd3d9a50fff0b4fca20ee
Akmal Rizayev <akmaljon.rizayev@gmail.com>
2022-03-04 13:43:19 +0500

    Multiple-record SQL Queries

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,7 +3,6 @@ package main
 import (
 	"errors" // New import
 	"fmt"
-	"html/template"
 	"net/http"
 	"strconv"
 
@@ -18,29 +17,39 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	files := []string{
-		"./ui/html/home.page.tmpl",
-		"./ui/html/base.layout.tmpl",
-		"./ui/html/footer.partial.tmpl",
-	}
-
-	ts, err := template.ParseFiles(files...)
+	s, err := app.snippets.Latest()
 	if err != nil {
-		// Because the home handler function is now a method against application
-		// it can access its fields, including the error logger. We'll write the log
-		// message to this instead of the standard logger.
-		app.errorLog.Println(err.Error())
-		app.serverError(w, err) // Use the serverError() helper.
+		app.serverError(w, err)
 		return
 	}
 
-	err = ts.Execute(w, nil)
-	if err != nil {
-		// Also update the code here to use the error logger from the application
-		// struct.
-		app.errorLog.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+	for _, snippet := range s {
+		fmt.Fprintf(w, "%v\n", snippet)
 	}
+
+	// files := []string{
+	// 	"./ui/html/home.page.tmpl",
+	// 	"./ui/html/base.layout.tmpl",
+	// 	"./ui/html/footer.partial.tmpl",
+	// }
+
+	// ts, err := template.ParseFiles(files...)
+	// if err != nil {
+	// 	// Because the home handler function is now a method against application
+	// 	// it can access its fields, including the error logger. We'll write the log
+	// 	// message to this instead of the standard logger.
+	// 	app.errorLog.Println(err.Error())
+	// 	app.serverError(w, err) // Use the serverError() helper.
+	// 	return
+	// }
+
+	// err = ts.Execute(w, nil)
+	// if err != nil {
+	// 	// Also update the code here to use the error logger from the application
+	// 	// struct.
+	// 	app.errorLog.Println(err.Error())
+	// 	http.Error(w, "Internal Server Error", 500)
+	// }
 }
 
 // Change the signature of the showSnippet handler so it is defined as a method
